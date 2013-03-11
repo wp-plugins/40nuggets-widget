f0d57b2cbc7c48795c398365dac98fae49d21932
eliav@40nuggets.com <eliav@40nuggets.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 20:25:29 +0000

    rolling back to 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/40nuggets-widget@680005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/admin/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$blog_name = get_option('blogname');
-	$blog_name = "this is the BEST web@#$site Ever created #10 check us@!#%!";
-	$username = preg_replace("/[^a-zA-Z0-9]/", "", $blog_name);
-	$username = substr($username,0,20);
-	$username = strtolower($username);
-	echo "$username@40nuggets.com";
- ?>
